d19f489a9d87448916f71d8134cf35f9a06afa3c
tyoooardan <tyoardanmi@gmail.com>
2025-04-25 20:05:06 +0700

    Add files via upload

---
+++ pertemuan9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	string kata;
+	cout << "Masukkan kata = HALO"<<endl;
+	cin>>kata;
+	
+	if (kata == "HALO"){
+		cout << "Kata yang dimasukkan sesuai"<<endl;
+	}else{
+		cout << "Kata yang dimasukkan tidak sesuai "<<endl;
+	}
+}
